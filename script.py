6aa2ee0bb8b5b893619b77205e1375fdd8b36dae
Martin <mvalencia1350@arizona.edu>
2023-09-16 15:17:15 -0700

    Create core functionality

--- script.py
+++ script.py
@@ -1,15 +1,52 @@
+from email import header
 import json
-import os
+import csv
+#from collections import OrderedDict
 
-f = open("data/SampleData_01.json", "r")
-data_path = ".//data"
-dir_list = os.listdir(data_path)
+data_path = "./data"
+sample_data_01 = "./sample_data/SampleData_01.json"
+sample_data_02 = "./data/Berlin.json"
+tech_path = "./tech.txt"
 
-data = json.load(f)
+def main():
+    # Collect a list of headers from tech.txt
+    tech_file = open(tech_path, "r")
+    headers =  tech_file.read().split()
+    headers.insert(0, "City")
 
-for i in data:
-    print(i["description"])
+    tech_dict = {headers[0]: ""}
+    tech_dict.update({skill: 0 for skill in headers[1:]}) 
 
-print(dir_list)
 
-f.close()
+    print(headers)
+    print(tech_dict)
+
+    # DictWriter Example
+    with open('results.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+
+        writer.writeheader()
+
+        tech_dict["City"] = "Madrid"
+        # Open data file with json library
+        # Note: this is for only one file, we gotta loop
+        f = open(sample_data_02, "r", encoding='utf-8')
+        data = json.load(f)
+
+        for post in data:
+            for k in range(1,len(tech_dict)):
+
+                if headers[k].lower() in post["description"].lower():
+                    tech_dict[headers[k]] += 1
+
+            #print(i["description"])
+            #print("software" in i["description"])
+        writer.writerow(tech_dict)
+        # writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+        # writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+
+    f.close()
+    tech_file.close()
+
+if __name__ == "__main__":
+    main()
